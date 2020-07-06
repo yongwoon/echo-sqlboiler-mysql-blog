fb5c68429fe35d643927dac2b91a06e4ba1ab58e
yongwoon kim <yonyon.japan@gmail.com>
2020-07-06 12:45:15 +0900

    Set routing && api/v1/posts, api/v1/posts/:id

--- api/main.go
+++ api/main.go
@@ -1,15 +1,13 @@
 package main
 
 import (
-	"net/http"
-
 	"github.com/labstack/echo/v4"
+	"github.com/yongwoon/echo-blog/route"
 )
 
 func main() {
 	e := echo.New()
-	e.GET("/", func(c echo.Context) error {
-		return c.String(http.StatusOK, "Hello, World!")
-	})
+	route.Init(e)
+
 	e.Logger.Fatal(e.Start(":1323"))
 }
