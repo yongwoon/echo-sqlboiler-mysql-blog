dc4fde98e8cc190230fc248edb69547d16fd2aeb
yongwoon kim <yonyon.japan@gmail.com>
2020-07-06 12:25:20 +0900

    create posts migration file

---
+++ api/db/migration/20200706032041-create_post.sql
@@ -0,0 +1,13 @@
+
+-- +migrate Up
+CREATE TABLE posts (
+  `id` bigint(20) unsigned NOT NULL,
+  `title` varchar(255) NOT NULL,
+  `body` text NOT NULL,
+  `created_at` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `updated_at` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- +migrate Down
+DROP TABLE posts;
